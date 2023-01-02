e2cacdd9623a37907ff0220ec4b0370b6872bf48
gabriel-martins96 <gbl.martins96@gmail.com>
2023-01-02 20:52:53 -0300

    upp

---
+++ Seção 6/Pilha/Pilha.java
@@ -0,0 +1,21 @@
+package Pilha;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Pilha {
+
+	public static void main(String[] args) {
+		Deque<String> livros = new ArrayDeque<>();
+		
+		//add) add (retorna) ou push (não retorna nada)
+		//buscam)peek e element -> buscam, mas não removem o último (LiFo)
+		//remoção) poll e remove, pop também remove
+		
+		//Outros métodos: size, clear, isEmpty, poll (chama e remove da fila), contains...
+		
+		
+		
+	}
+
+}
